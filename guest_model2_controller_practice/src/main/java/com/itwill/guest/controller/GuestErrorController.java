6a1eb069fac1dd86a579b7f478be0ecd60c01487
ddZoebb <jjushine24@naver.com>
2022-08-11 18:08:56 +0900

    ..

--- guest_model2_controller_practice/src/main/java/com/itwill/guest/controller/GuestErrorController.java
+++ guest_model2_controller_practice/src/main/java/com/itwill/guest/controller/GuestErrorController.java
@@ -1,17 +1,17 @@
-package com.itwill.user.controller;
+package com.itwill.guest.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.itwill.summer.Controller;
 
-public class UserMainController implements Controller{
+public class GuestErrorController implements Controller{
 
 	@Override
 	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		HttpSession session=request.getSession();
-		return "forward:/WEB-INF/views/user_main.jsp";
+		String forwardPath="";
+		forwardPath="forward:/WEB-INF/views/guest_error.jsp";
+		return forwardPath;
 	}
 
 }
---
+++ guest_model2_controller_practice/src/main/java/com/itwill/guest/controller/GuestWriteFormController.java
@@ -0,0 +1,29 @@
+package com.itwill.guest.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.itwill.guest.GuestService;
+import com.itwill.summer.Controller;
+
+/*
+ * - 클라이언트요청한개를 처리하는 비즈니스로직을 담고있는 객체
+ * - DispatcherServlet객체가 호출하는 handleRequest메쏘드를가짐
+ * - handleRequest메쏘드가호출되면 DispatcherServlet객체에 forwardPath를 반환해줌
+ */
+public class GuestWriteFormController implements Controller {
+
+	public GuestWriteFormController() {
+	}
+
+	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+		String forwardPath = "";
+		try {
+		forwardPath = "forward:/WEB-INF/views/guest_write_form.jsp";
+		}catch (Exception e) {
+			e.printStackTrace();
+			forwardPath = "forward:/WEB-INF/views/guest_error.jsp";
+		}
+		return forwardPath;
+	}
+}
